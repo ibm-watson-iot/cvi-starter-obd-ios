004ac8576dcc75bbfd7b25df69966f439246c35b
Eliad Moosavi <iliadm@ca.ibm.com>
2016-11-14 16:05:48 -0500

    Initial Commit

---
+++ starter.automotive.obdii/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  starter.automotive.obdii
+//
+//  Created by Eliad Moosavi on 2016-11-14.
+//  Copyright © 2016 IBM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
