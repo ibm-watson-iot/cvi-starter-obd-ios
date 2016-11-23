db416e6e4c2cf8bc519e7ef0cba5743b3d221a81
Eliad Moosavi <iliadm@ca.ibm.com>
2016-11-23 14:38:51 -0500

    Tell user that "Real OBDII connection" is coming soon, and let them try the simulator or exit

--- starter.automotive.obdii/ViewController.swift
+++ starter.automotive.obdii/ViewController.swift
@@ -102,11 +102,23 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     private func actualDevice() {
         let alertController = UIAlertController(title: "Are you connected to your OBDII Dongle?", message: "You need to connect to your OBDII dongle through Wi-Fi, and then press \"Yes\"", preferredStyle: UIAlertControllerStyle.alert)
         alertController.addAction(UIAlertAction(title: "Yes", style: UIAlertActionStyle.default) { (result : UIAlertAction) -> Void in
-            print("OK")
+            let alertController = UIAlertController(title: "Coming Soon", message: nil, preferredStyle: UIAlertControllerStyle.alert)
+            
+            alertController.addAction(UIAlertAction(title: "Try the simulator", style: UIAlertActionStyle.default) { (result : UIAlertAction) -> Void in
+                self.startApp()
+            })
+            
+            alertController.addAction(UIAlertAction(title: "Exit", style: UIAlertActionStyle.destructive) { (result : UIAlertAction) -> Void in
+                exit(0)
+            })
+            
+            self.present(alertController, animated: true, completion: nil)
         })
         alertController.addAction(UIAlertAction(title: "Cancel", style: UIAlertActionStyle.destructive) { (result : UIAlertAction) -> Void in
             let toast = UIAlertController(title: nil, message: "You would need to connect to your OBDII dongle in order to use this feature!", preferredStyle: UIAlertControllerStyle.alert)
+            
             self.present(toast, animated: true, completion: nil)
+            
             DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
                 exit(0)
             }
