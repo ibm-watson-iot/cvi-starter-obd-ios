4576c5c43206b9fd14846ebfe9c16c02b02751bd
kws <kws@jp.ibm.com>
2017-02-17 15:45:08 +0900

    Update URL of license in source code header

--- starter.automotive.obdii/MQTTConnection.swift
+++ starter.automotive.obdii/MQTTConnection.swift
@@ -1,11 +1,12 @@
-//
-//  MQTTConnection.swift
-//  starter.automotive.obdii
-//
-//  Created by Eliad Moosavi on 2016-12-14.
-//  Copyright © 2016 IBM. All rights reserved.
-//
-
+/**
+ * Copyright 2016 IBM Corp. All Rights Reserved.
+ *
+ * Licensed under the IBM License, a copy of which may be obtained at:
+ *
+ * http://www14.software.ibm.com/cgi-bin/weblap/lap.pl?li_formnum=L-DDIN-AHKQ8X&popup=n&title=IBM%20IoT%20for%20Automotive%20Sample%20Starter%20Apps%20%28iOS%20Mobile%29
+ *
+ * You may not use this file except in compliance with the license.
+ */
 import CocoaMQTT
 
 protocol MQTTConnectionDelegate: class {
